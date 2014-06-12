52b085440d412fcc1550bb875060215589a444d4
rootn3rd <krishnahare14@gmail.com>
2014-06-13 01:34:07 +0530

    1112. Number Steps
    http://www.spoj.com/problems/NSTEPS/
    
    Initial Draft

---
+++ SPOJChallenges/SPOJChallenges/NSTEPS.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SPOJChallenges
+{
+    class NSTEPS
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
